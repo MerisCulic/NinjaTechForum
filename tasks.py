e88a6edcd9d09b5c5a20732f38f580026004f065
Meris Čulić <meris.culic@gmail.com>
2020-09-17 09:30:55 +0200

    Add task for sending emails

--- tasks.py
+++ tasks.py
@@ -1,16 +1,44 @@
 import os
-import random
+import json
+import requests
 from huey import RedisHuey
 
 huey = RedisHuey(url=os.getenv("REDIS_URL"))
 
-@huey.task(retries=5, retry_delay=5)
-def get_random_num():
-    print("This is a task to get a random number")
-    num = random.randint(1, 3)
-    print("Random number is {}.".format(num))
 
-    if num == 1:
-        return True
+@huey.task(retries=10, retry_delay=600)
+def send_email_task(receiver_email, subject, text):
+    sender_email = os.getenv("MY_SENDER_EMAIL")
+    api_key = os.getenv('SENDGRID_API_KEY')
+
+    if sender_email and api_key:
+        url = "https://api.sendgrid.com/v3/mail/send"
+
+        data = {"personalizations": [{
+                    "to": [{"email": receiver_email}],
+                    "subject": subject
+                }],
+
+                "from": {"email": sender_email},
+
+                "content": [{
+                    "type": "text/plain",
+                    "value": text
+                }]
+        }
+
+        headers = {
+            'authorization': "Bearer {0}".format(api_key),
+            'content-type': "application/json"
+        }
+
+        response = requests.request("POST", url=url, data=json.dumps(data), headers=headers)
+
+        print("Sent to SendGrid")
+        print(response.text)
     else:
-        raise Exception("Error in the worker.. :(")
+        print("No env vars or no email address.")
+        print("The email was not sent.")
+        print("If it was sent, this would be the subject: {}".format(subject))
+        print("This would be the text: {}".format(text))
+        print("And this would be the receiver: {}".format(receiver_email))
--- utils/email_helper.py
+++ utils/email_helper.py
@@ -1,40 +1,18 @@
-import json
 import os
-import requests
+from tasks import send_email_task
 
 
 def send_email(receiver_email, subject, text):
-    sender_email = os.getenv("MY_SENDER_EMAIL")
-    api_key = os.getenv("SENDGRID_API_KEY")
 
-    if not api_key:
-        print("No api")
-
-    if not sender_email:
-        print("No email address")
+    if os.getenv('REDIS_URL'):
+        # if the web app is on Heroku, call the "send email" task
+        send_email_task(receiver_email, subject, text)
 
     else:
-        url = "https://api.sendgrid.com/v3/mail/send"
-
-        data = {"personalizations": [{
-                    "to": [{"email": receiver_email}],
-                    "subject": subject
-                }],
-
-                "from": {"email": sender_email},
-
-                "content": [{
-                    "type": "text/plain",
-                    "value": text
-                }]
-                }
-
-        headers = {
-            'authorization': "Bearer {0}".format(api_key),
-            'content-type': "application/json"
-        }
-
-        response = requests.request("POST", url=url, data=json.dumps(data), headers=headers)
-
-        print("Sent to SendGrid")
-        print(response.text)
+        # but if you're on localhost, just simulate sending email in the console
+        print("You are on localhost, so the email will not be really sent.")
+        print("---------------EMAIL MESSAGE----------------")
+        print("Email recipient: {}".format(receiver_email))
+        print("Subject: {}".format(subject))
+        print(text)
+        print("---------------EMAIL MESSAGE----------------")
